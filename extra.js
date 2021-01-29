8cbb5a751cf2132799097000873e89da1ce7c1c0
max <mkru055@gmail.com>
2021-01-29 15:24:04 +0300

    new commit ES6

---
+++ extra.js
@@ -0,0 +1,21 @@
+// export let name = 'maxs'
+// export let age = 20
+
+// let name = 'maxs'
+// let age = 20
+//
+// export {
+//     age,
+//     name
+// }
+
+// export default function func(){
+//     console.log('im func')
+// }
+
+
+// export class Car {
+//     constructor(mark) {
+//         console.log(mark)
+//     }
+// }
--- index.js
+++ index.js
@@ -163,21 +163,256 @@
 // console.log('set has', set.has(2))
 
 
-let set = new WeakSet()
-
-let key = {}
-set.add(key)
-console.log(set.size)
-key = null
-console.log(set.size)
-
-
+// let set = new WeakSet()
+//
+// let key = {}
+// set.add(key)
+// console.log(set.size)
+// key = null
+// console.log(set.size)
 
 
+//--------------------------обьекты Map/weakMap
+
+// let map = new Map()
+//
+// map.set('name', 'max')
+// map.set('age', 20)
+
+// console.log(map.get('age'))
+
+// let obj1 = {}
+// let obj2 = {}
+//
+// map.set(obj1, 10)
+// map.set(obj2, 50)
+//
+// console.log('size', map.size)
+// console.log('has', map.has(obj2))
+// console.log('delete', map.delete(obj2))
+// console.log('size', map.size)
+// console.log('has', map.has(obj2))
 
 
+// let map = new Map([
+//     ['name', 'max'],
+//     ['age', 20]
+// ])
+// for(let val of map.values()){
+//     console.log('values', val)
+// }
+//
+// for(let val of map.keys()){
+//     console.log('values', val)
+// }
+//
+// for(let entr of map.entries()){
+//     console.log(`${entr[0]}-->${entr[1]}`)
+// }
+
+// let map  = new WeakMap();
+// let key = {}
+// map.set(key, 'key')
+// console.log('size ', map.size)
+// key = null
+// console.log('size ', map.size)
 
 
+//--------------------------система модулей. Модули
+
+
+// import * as extra from './extra'
+// import {name as n, age} from "./extra";
+//
+// console.log(n, age)
+
+// import func from './extra'
+//
+// func()
+//
+// import {Car} from "./extra";
+//
+// let car = new Car('bmw');
+
+//--------------------------symbols
+// let s = Symbol('field')
+// let s2 = Symbol('1')
+// let s3 = Symbol('1')
+// console.log(typeof s2 === typeof s3)
+// console.log( s2 === s3) // false -> unique
+
+// let obj = {
+//     age: 20,
+//     [s]: 'max'
+// }
+//
+// console.log(Object.getOwnPropertySymbols(obj))
+
+// let num = 1;
+// let str = '2'
+// let arr = [1, 2]
+// let obj = {name: 'max'}
+//
+// console.log('Number', typeof num[Symbol.iterator])
+// console.log('String', typeof str[Symbol.iterator])
+// console.log('Array', typeof arr[Symbol.iterator])
+// console.log('Object', typeof obj[Symbol.iterator])
+
+// Number undefined
+// String function
+// Array function
+// Object undefined
+
+// function createIterator(arr){
+//     let count = 0
+//     return {
+//         next() {
+//             return count < arr.length
+//                 ? { value: arr[count++], done: false}
+//                 : { value: undefined, done: true}
+//         }
+//     }
+// }
+// let iter = createIterator([1,2,3,4])
+// console.log(iter.next())
+// console.log(iter.next())
+// console.log(iter.next())
+// console.log(iter.next())
+// console.log(iter.next())
+
+// 1 1 2 3 5 8
+
+// let fib = {
+//     [Symbol.iterator](){
+//         let pre = 0, cur = 1
+//         return{
+//             next(value) {
+//                 [pre, cur] = [cur, pre + cur]
+//                 return {value: cur, done: false}
+//             }
+//         }
+//     }
+// }
+// for( let n of fib){
+//     if( n > 100) break
+//     console.log(n)
+// }
+
+//------------------------ Генераторы
+
+// function* gen(){
+//     yield 11
+//     yield 22
+//     yield 33
+// }
+//
+// let iter = gen()
+//
+// console.log(iter.next())
+// console.log(iter.next())
+// console.log(iter.next())
+// console.log(iter.next())
+
+
+// function* g1(){
+//     yield 1
+//     yield* g2()
+//     yield 4
+// }
+// function* g2(){
+//     yield 2
+//     yield 3
+// }
+//
+// let iter = g1()
+
+// console.log(iter.next())
+
+
+// function* getRange(start = 0, end = 100, step = 10){
+//     while (start < end){
+//         yield start
+//         start+=step
+//     }
+// }
+// for(let n of getRange(10, 50,3)){
+//     console.log(n)
+// }
+
+// let fib = {
+//     *[Symbol.iterator](){
+//         let pre = 0, cur = 1
+//         for(;;){
+//                 [pre, cur] = [cur, pre + cur]
+//                 yield cur
+//         }
+//     }
+// }
+// for( let n of fib){
+//     if( n > 100) break
+//     console.log(n)
+// }
+
+//-------------------new methods
+
+// Object.assign()
+// .find(x => x>3)
+// str.repeat(3)
+// str.startsWith('hel')
+
+//------------------Promise
+
+//old es5
+// function oldDelay(ms, func){
+//     setTimeout(function (){
+//         func()
+//     }, ms)
+// }
+// oldDelay(3000, function (){
+//     console.log('old delay passed')
+// }
+
+// function delay(ms = 1000){
+//     return new Promise((resolve, reject)=> {
+//         setTimeout(()=>{
+//             resolve()
+//         }, ms)
+//     })
+// }
+//
+// delay(2000)
+//     .then(()=>{
+//     console.log('new delay passed')
+// })
+//     .catch(()=>{
+//         console.info('error')
+//     })
+
+
+import $ from 'jquery'
+let promise = new Promise((resolve, reject)=>{
+    $.ajax({
+        url: 'http://date.jsontest.com',
+        dataType: 'json',
+        success:function (response){
+        resolve(response)
+        },
+        error: function (error){
+        reject(error)
+        }
+    })
+})
+
+promise
+    .then((data)=>{
+    return data.date
+})
+    .then((date)=>{
+        console.log('date', date)
+    })
+    .catch((error)=>{
+        console.info(error)
+    })
 
 
 
